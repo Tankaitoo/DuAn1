c0bc7884ae526433cae0788486d80f603fdb4d15
Tankaitoo <phanphuoctan11c6@gmail.com>
2024-11-10 12:12:30 +0700

    Add models file

---
+++ models/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "localhost";
+$db_username = "root";
+$db_password = "myspl";
+$db_database = "database";
+$connection = new  myspl(
+    $server,
+    $db_username,
+    $db_password,
+    $db_database
+);
+
+var_dump(connection);
